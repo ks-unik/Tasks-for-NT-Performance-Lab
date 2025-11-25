a7aea287fd1da066103c5d9de9d1de32f332c97a
Kseniia <k.alex1234@yandex.ru>
2025-11-25 13:02:15 +0300

    Обновлен файл task-4.py

--- task1/task-1.py
+++ task1/task-1.py
@@ -3,25 +3,20 @@ data_entry = input()
 data_entry_list = data_entry.split()
 n1, m1, n2, m2 = map(int, data_entry_list)
 
-array_1 = list(range(1, n1 + 1))
-array_2 = list(range(1, n2 + 1))
-path_1 = [1]
-path_2 = [1]
+def get_path(n, m):
+    path = [1]
+    current = 1
+    while True:
+        for _ in range(m - 1):
+            current += 1
+            if current > n:
+                current = 1
+        if current == 1:
+            break
+        path.append(current)
+    return path
 
-interval_1 = m1 - 1
-interval_2 = m2 - 1
+path_1 = get_path(n1, m1)
+path_2 = get_path(n2, m2)
 
-while True:
-    last_number_1 = path_1[-1]
-    next_number_1 = (last_number_1 + interval_1 - 1) % n1 + 1
-    if next_number_1 == 1:
-        break
-    path_1.append(next_number_1)
-print(path_1)
-while True:
-    last_number_2 = path_2[-1]
-    next_number_2 = (last_number_2 + interval_2 - 1) % n2 + 1
-    if next_number_2 == 1:
-        break
-    path_2.append(next_number_2)
-print(path_2)
+print(*path_1, *path_2)
