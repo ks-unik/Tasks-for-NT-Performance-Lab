f96c3313ed5bd576ef3093a06b3c5aa9f6194075
Kseniia <k.alex1234@yandex.ru>
2025-11-27 19:28:40 +0300

    Обновлён файл task-3.py с исправлениями по тестовому заданию

--- task3/task-3.py
+++ task3/task-3.py
@@ -1,12 +1,6 @@
 import json
 import sys
 
-def build_index(obj_list, index):
-    for obj in obj_list:
-        index[obj["id"]] = obj
-        if "values" in obj:
-            build_index(obj["values"], index)
-
 if len(sys.argv) < 4:
     print("Необходимо указать 3 файла")
     sys.exit(1)
@@ -15,28 +9,23 @@ filename_1 = sys.argv[1]
 filename_2 = sys.argv[2]
 filename_3 = sys.argv[3]
 
-with open(filename_1, 'r', encoding='utf-8') as f_1:
-    values_json = json.load(f_1)
+with open(filename_1, 'r') as f:
+    values_json = json.load(f)
 
-with open(filename_2, 'r', encoding='utf-8') as f_2:
-    tests_json = json.load(f_2)
+with open(filename_2, 'r') as f:
+    tests_json = json.load(f)
 
-index_values = {}
-build_index(values_json["values"], index_values)
+index_values = {obj["id"]: obj for obj in values_json["values"]}
 
-index_tests = {}
-build_index(tests_json["tests"], index_tests)
+def fill_values(obj_list, index):
+    for obj in obj_list:
+        obj_id = obj["id"]
+        if obj_id in index:
+            obj["value"] = index[obj_id]["value"]
+        if "values" in obj:
+            fill_values(obj["values"], index)
 
-for id_value, obj_from_new in index_tests.items():
-    if id_value in index_values:
-        obj_from_values = index_values[id_value]
-        for key, value in obj_from_new.items():
-            if key not in obj_from_values or not obj_from_values[key]:
-                obj_from_values[key] = value
-    else:
-        index_values[id_value] = obj_from_new
+fill_values(tests_json["tests"], index_values)
 
-values_json["values"] = list(index_values.values())
-
-with open(filename_3, 'w', encoding='utf-8') as f_3:
-    json.dump(values_json, f_3, ensure_ascii=False, indent=4)
+with open(filename_3, 'w', encoding='utf-8') as f:
+    json.dump(tests_json, f, ensure_ascii=False, indent=4)
