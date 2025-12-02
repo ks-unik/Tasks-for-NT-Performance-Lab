e2f4b94daae5a65cb03da6165c39e938adc859b9
ks-unik <ks.a.filippova@yandex.ru>
2025-12-02 21:21:21 +0300

    Update task-3.py

--- task3/task-3.py
+++ task3/task-3.py
@@ -1,38 +1,37 @@
 import json
 import sys
 
-def build_index(obj_list, index):
-    for obj in obj_list:
-        index[obj["id"]] = obj
-        if "values" in obj:
-            build_index(obj["values"], index)
-
 if len(sys.argv) < 4:
-    print("Необходимо указать 3 файла")
+    print("Необходимо указать имя 3-х файлов в аргументе командной строки")
     sys.exit(1)
 
 filename_1 = sys.argv[1]
-filename_2 = sys.argv[2]
-filename_3 = sys.argv[3]
-
-with open(filename_1, 'r') as f:
+with open(filename_1, "r") as f:
     values_json = json.load(f)
 
-with open(filename_2, 'r') as f:
-    tests_json = json.load(f)
+filename_2 = sys.argv[2]
+with open(filename_2, "r") as f:
+    tests = json.load(f)
 
-index_values = {obj["id"]: obj for obj in values_json["values"]}
+def node(dict_test, dict_values):
+    if dict_test["id"] in dict_values:
+        dict_test["value"] = dict_values[dict_test["id"]]
 
+    if "values" not in dict_test:
+        return
+    
+    elif not dict_test["values"]:
+        return
+    
+    else:
+        for child in dict_test["values"]:
+            node(child, dict_values)
 
-def fill_values(obj_list, index):
-    for obj in obj_list:
-        obj_id = obj["id"]
-        if obj_id in index:
-            obj["value"] = index[obj_id]["value"]
-        if "values" in obj:
-            fill_values(obj["values"], index)
+values_map = {item["id"]: item["value"] for item in values_json}
 
-fill_values(tests_json["tests"], index_values)
+for test_node in tests["tests"]:
+    node(test_node, values_map)
 
-with open(filename_3, 'w', encoding='utf-8') as f:
-    json.dump(tests_json, f, ensure_ascii=False, indent=4)
+filename_3 = sys.argv[3]
+with open(filename_3, "w") as f:
+    json.dump(tests, f)
